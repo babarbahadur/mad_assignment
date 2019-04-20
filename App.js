ffd603fb49fe5ea196d63b8f47f2c2a3e20c5dd0
Babar <babar.bahadur@hotmail.com>
2019-04-20 22:26:37 +0500

    My second commit

--- App.js
+++ App.js
@@ -1,58 +1,134 @@
 import React, { Component } from 'react';
-import { AppRegistry, Image, View, Text, StyleSheet, TextInput, TouchableOpacity, ScrollView , Button} from 'react-native';
+import { Text, View, Button, Image } from 'react-native';
 
-import Banana from './app/pages/banana';
-import BlinkApp from './app/pages/BlinkApp'
-import LotsOfStyles from './app/pages/LotsOfStyles'
-import FixedDimensionsBasic from './app/pages/FixedDimensionsBasic'
-import FlexDirectionBasics from './app/pages/FlexDirectionBasics'
-import PizzaTranslator from './app/pages/PizzaTranslator'
-import IScrolledDownAndWhatHappenedNextShockedMe from './app/pages/IScrolledDownAndWhatHappenedNextShockedMe'
-import FlatListBasics from './app/pages/FlatListBasics'
-import FetchExample from './app/pages/FetchExample'
+//Navigation Imports
+import { createStackNavigator, createAppContainer,  } from 'react-navigation';
 
-export default class Bananas extends Component {
+
+//Home Screen View Settings
+class Introduction   extends Component {
+  static navigationOptions = {
+    title: 'Introduction'
+  }
   render() {
-    
+
     return (
-         
-      <ScrollView>
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      
+      <Image
+          style={{width: 200, height: 200}}
+          source={{uri: 'https://pbs.twimg.com/profile_images/1046406628755611648/TRyMT7km.jpg'}}
+        />
 
-      <Banana />
-      <BlinkApp/>
-      <LotsOfStyles/>
-      <FixedDimensionsBasic/>
-      <FlexDirectionBasics/>
-      <PizzaTranslator />
-      <IScrolledDownAndWhatHappenedNextShockedMe />
-      <FlatListBasics />
-      <FetchExample />
+        <Text>Obtain a challenging leadership position applying creative problem solving and lean management skills with a growing company to achieve optimum utilization of its resources and maximum profits</Text>
+      
+        <Button
+          title="Next"
+          onPress={() => this.props.navigation.navigate('Education', {
+            itemId: 12,
+            otherMsg: "Sent to Education"
+          })}
+        />
+      </View>
 
-     </ScrollView>
-
-    <Button
-      onPress={() => {
-      Alert.alert('Why you tapped the button?');
-}}
-    title="Do not Press Me"
-    />
-     
+      
 
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
 
-  Text: {
-    fontSize: 18,
-    color: 'green',
+//Education Screen View Settings
+class EducationScreen extends Component {
+  static navigationOptions = {
+    title: 'Education Screen'
   }
- 
-});
+  render() {
+    const { navigation } = this.props;
+    const itemId = navigation.getParam('itemId', 'NO-ID');
+    const otherMsg = navigation.getParam('otherMsg', 'This is the default Msg');
+
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Matric: VM Public School</Text>
+        <Text>FSC: Sindh Muslim Goverment Science College</Text>
+        <Text>Graduation: BS-SE (Mohammad Ali Jinnah University)</Text>
+        
+        
+
+        <Button
+          title="Next"
+          onPress={() => this.props.navigation.navigate('Experience')}
+        />
+        
+        <Button
+          title="Home"
+          onPress={() => this.props.navigation.navigate('Home')}
+        />
+
+        <Button
+          title="Go back"
+          onPress={() => this.props.navigation.goBack()}
+        />
+
+      </View>
+    );
+  }
+}
+
+
+
+
+
+//Experience Screen View Settings
+class ExperienceScreen extends Component {
+  static navigationOptions = {
+    title: 'Experience Screen'
+  }
+  render() {
+    const { navigation } = this.props;
+    const itemId = navigation.getParam('itemId', 'NO-ID');
+    const otherMsg = navigation.getParam('otherMsg', 'This is the default Msg');
+
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Fresh Graduate</Text>
+        
+        <Button
+          title="Home"
+          onPress={() => this.props.navigation.navigate('Home')}
+        />
+
+        <Button
+          title="Go back"
+          onPress={() => this.props.navigation.goBack()}
+        />
+
+      </View>
+    );
+  }
+}
+
+
+
+
+
+//Creation of the Stack Navigator
+const AppNavigator = createStackNavigator({
+  Home: Introduction,
+  Education: EducationScreen,
+  Experience: ExperienceScreen
+},
+  {
+    initialRouteName: "Home"
+  });
+
+//Exporting while creating the App Container
+// export default createAppContainer(AppNavigator);
+const AppContainer = createAppContainer(AppNavigator);
+export default class App extends Component {
+  render() {
+    return <AppContainer />
+  }
+}
+
